147a1f1549cd13df1a20100e9cb2f7b660492e69
Saori Sakai <124588744+tyatewo@users.noreply.github.com>
2023-03-31 09:13:11 +0000

    bootstrap

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -11,3 +11,13 @@ environment.plugins.prepend(
     Popper: 'popper.js'
   })
 )
+
+const webpack = require('webpack')
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: 'popper.js'
+  })
+)
